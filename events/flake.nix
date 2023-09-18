7360c37f3581abfe7ed8bef5d3ba2deddcdb42bd
rydnr <github@acm-sl.org>
2023-09-18 16:22:50 +0200

    Updated pythoneda-shared-pythoneda/domain-artifact to 0.0.1a42
    Added python 3.11

--- events/flake.nix
+++ events/flake.nix
@@ -23,7 +23,7 @@
     flake-utils.url = "github:numtide/flake-utils/v1.0.0";
     pythoneda-shared-artifact-changes-shared = {
       url =
-        "github:pythoneda-shared-artifact-changes/shared-artifact/0.0.1a13?dir=shared";
+        "github:pythoneda-shared-artifact-changes/shared-artifact/0.0.1a14?dir=shared";
       inputs.nixos.follows = "nixos";
       inputs.flake-utils.follows = "flake-utils";
       inputs.pythoneda-shared-pythoneda-banner.follows =
@@ -33,7 +33,7 @@
     };
     pythoneda-shared-code-requests-shared = {
       url =
-        "github:pythoneda-shared-code-requests/shared-artifact/0.0.1a9?dir=shared";
+        "github:pythoneda-shared-code-requests/shared-artifact/0.0.1a10?dir=shared";
       inputs.nixos.follows = "nixos";
       inputs.flake-utils.follows = "flake-utils";
       inputs.pythoneda-shared-pythoneda-banner.follows =
@@ -48,7 +48,7 @@
     };
     pythoneda-shared-pythoneda-domain = {
       url =
-        "github:pythoneda-shared-pythoneda/domain-artifact/0.0.1a41?dir=domain";
+        "github:pythoneda-shared-pythoneda/domain-artifact/0.0.1a42?dir=domain";
       inputs.nixos.follows = "nixos";
       inputs.flake-utils.follows = "flake-utils";
       inputs.pythoneda-shared-pythoneda-banner.follows =
@@ -155,7 +155,7 @@
         devShells = rec {
           default = pythoneda-shared-code-requests-events-default;
           pythoneda-shared-code-requests-events-default =
-            pythoneda-shared-code-requests-events-python310;
+            pythoneda-shared-code-requests-events-python311;
           pythoneda-shared-code-requests-events-python38 = shared.devShell-for {
             package = packages.pythoneda-shared-code-requests-events-python38;
             python = pkgs.python38;
@@ -185,11 +185,22 @@
                 pythoneda-shared-pythoneda-domain.packages.${system}.pythoneda-shared-pythoneda-domain-python310;
               inherit archRole layer nixpkgsRelease org pkgs repo space;
             };
+          pythoneda-shared-code-requests-events-python311 =
+            shared.devShell-for {
+              package =
+                packages.pythoneda-shared-code-requests-events-python311;
+              python = pkgs.python311;
+              pythoneda-shared-pythoneda-banner =
+                pythoneda-shared-pythoneda-banner.packages.${system}.pythoneda-shared-pythoneda-banner-python311;
+              pythoneda-shared-pythoneda-domain =
+                pythoneda-shared-pythoneda-domain.packages.${system}.pythoneda-shared-pythoneda-domain-python311;
+              inherit archRole layer nixpkgsRelease org pkgs repo space;
+            };
         };
         packages = rec {
           default = pythoneda-shared-code-requests-events-default;
           pythoneda-shared-code-requests-events-default =
-            pythoneda-shared-code-requests-events-python310;
+            pythoneda-shared-code-requests-events-python311;
           pythoneda-shared-code-requests-events-python38 =
             pythoneda-shared-code-requests-events-for {
               python = pkgs.python38;
@@ -219,6 +230,16 @@
                 pythoneda-shared-code-requests-shared.packages.${system}.pythoneda-shared-code-requests-shared-python310;
               pythoneda-shared-pythoneda-domain =
                 pythoneda-shared-pythoneda-domain.packages.${system}.pythoneda-shared-pythoneda-domain-python310;
+            };
+          pythoneda-shared-code-requests-events-python311 =
+            pythoneda-shared-code-requests-events-for {
+              python = pkgs.python311;
+              pythoneda-shared-artifact-changes-shared =
+                pythoneda-shared-artifact-changes-shared.packages.${system}.pythoneda-shared-artifact-changes-shared-python311;
+              pythoneda-shared-code-requests-shared =
+                pythoneda-shared-code-requests-shared.packages.${system}.pythoneda-shared-code-requests-shared-python311;
+              pythoneda-shared-pythoneda-domain =
+                pythoneda-shared-pythoneda-domain.packages.${system}.pythoneda-shared-pythoneda-domain-python311;
             };
         };
       });
